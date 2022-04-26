037de5b53ede418196c74fc93fc161e8feeefe30
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-04-26 19:03:58 +0500

    Add Homework002 Square

---
+++ homework001/Program.cs
@@ -0,0 +1,15 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+int squareB = b * b;
+if (a == squareB)
+{
+Console.WriteLine("Первое число является квадратом второго и равно " + squareB);
+}
+else
+{
+Console.WriteLine("Первое число  не является квадратом второго"); 
+}

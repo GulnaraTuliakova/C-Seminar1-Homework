7a85a181c4d4bdcd4a25e9ad0e39910e557d1e17
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-04-26 20:05:35 +0500

    Add homework003 %2

---
+++ homework003/Program.cs
@@ -0,0 +1,8 @@
+﻿//Выяснить является ли число чётным
+Console.WriteLine("Определим, является ли число четным");
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0)
+Console.WriteLine(a + " число четное");
+else
+Console.WriteLine(a + " число нечетное");

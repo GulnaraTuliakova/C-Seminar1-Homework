73b347c8f7c5ffc878c472f0e662242696174cca
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-04-26 22:45:04 +0500

    Add Homework004 from 1 to n

---
+++ Homework004/Program.cs
@@ -0,0 +1,14 @@
+﻿//Показать четные числа от 1 до N
+Console.WriteLine("Показать четные числа от 1 до n");
+Console.Write("Введите число n: ");
+int b =  int.Parse(Console.ReadLine());
+int a = 1;
+while (a >= 1 && a < b)
+{
+   if (a % 2 == 0)
+   {
+   Console.Write(a + " ");   
+   } 
+   a++;
+}
+

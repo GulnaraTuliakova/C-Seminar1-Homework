2c00be936ada2662652c223b806e88ac59434d5c
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-04-27 16:15:20 +0500

    Add homework09 Third number

--- Homework009/Program.cs
+++ Homework009/Program.cs
@@ -1,1 +1,15 @@
 ﻿//Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("");
+Console.Write("Введите число n: ");
+int num = int.Parse(Console.ReadLine());
+//int num2= num;
+  //while (num <= 1000)
+   //{
+   //  if (num2 <1000)
+   //  {
+   //    num2 = num2 / 10;
+   //  }
+  // Console.WriteLine(num); 
+  // }
+  
+
---
+++ Homework09/Program.cs
@@ -0,0 +1,34 @@
+﻿//Найти третью цифру числа или сообщить, что её нет
+Console.WriteLine("Найдем третью цифру в числе");
+Console.Write("Введите целое число n: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100 && num > -100)
+      {
+      Console.Write("В числе нет третьей цифры");
+      }
+if (num >= 100 && num < 1000)
+  {
+   Console.WriteLine("Третья цифра числа " + num % 10);  
+  }
+if (num >= 1000)
+      {
+        while (num >= 1000)
+            {
+            num = num / 10;
+            }
+            Console.WriteLine("Третья цифра числа " + num % 10); 
+      }
+          
+if (num <= -100 && num > -1000)
+  {
+   Console.WriteLine("Третья цифра числа " + (-num) % 10);  
+  }
+if (num <= -1000)
+      {
+        while (num <= -1000)
+            {
+            num = num / 10;
+            }
+            Console.WriteLine("Третья цифра числа " + (-num) % 10); 
+      }
+

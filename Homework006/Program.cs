22355cdb733cb20b452b5c472c0c1bc256b257ee
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-04-27 10:27:19 +0500

    Add homework006 Max number from 10 to 99

---
+++ Homework006/Program.cs
@@ -0,0 +1,25 @@
+﻿//Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+Console.WriteLine("Наша задача показать наибольшую цифру выбранного числа из отрезка от 10 до 99");
+Console.WriteLine("Программа выбирает любое число из указанного отрезка и......");
+ int num = new Random().Next(9,100);
+Console.WriteLine("Это число: " + num);
+int num1 = num / 10;
+int num2 = num % 10;
+if (num1 > num2)
+{
+ Console.WriteLine("Наибольшая цифра числа "  + num + ": " + num1);   
+}
+if (num2 > num1)
+{
+ Console.WriteLine("Наибольшая цифра числа "  + num + ": " + num2);   
+}
+if (num2 == num1)
+{
+ Console.WriteLine("Обе цифры числа "  + num + " равны. Это цифра: " + num2);   
+}
+
+
+
+
+

cfcc18a11e2c58e0268421e7362d1c6b4741e0e8
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-04-27 10:43:00 +0500

    Add homework007 Delete second number

---
+++ Homework007/Program.cs
@@ -0,0 +1,15 @@
+﻿//Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("Удалим вторую цифру в трехзначном числе");
+Console.Write("Введите положительное трехзначное число: ");
+int num =  int.Parse(Console.ReadLine());
+if (num > 99 && num < 1000)
+{
+int num1 = num / 100;
+int num3 = num % 10;
+Console.WriteLine("Число " + num + " без второй цифры превращается в число: " + num1 + num3 );
+}
+else
+{
+Console.WriteLine("Не верно введено число! Попробуй еще!");
+}
